100e474fb816d2532ef0ae17c6ecebd5b4276c32
NagisaHere <rw75000777@gmail.com>
2023-03-17 08:16:43 +1000

    first commit

---
+++ index.py
@@ -0,0 +1,9 @@
+hours = 5
+if hours < 4:
+    message = "You are working too little"
+elif hours > 10:
+    message = "You are working too hard!"
+else:
+    message = "Keep up the good work!"
+
+print(message)
---
+++ readme.md
@@ -0,0 +1,3 @@
+save me from this hell
+
+the application will prompt the user (employee) to enter the work week, the employee ID, employee name, amount of hours worked throughout the weekday, 
