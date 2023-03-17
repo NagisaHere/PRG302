07e5d07d4495e409f48ed36f4a2730bacd5083c5
NagisaHere <rw75000777@gmail.com>
2023-03-17 10:25:14 +1000

    add hour function completed

--- index.py
+++ index.py
@@ -1,9 +1,49 @@
-hours = 5
-if hours < 4:
-    message = "You are working too little"
-elif hours > 10:
-    message = "You are working too hard!"
-else:
-    message = "Keep up the good work!"
+# # THIS CODE IS NOT USABLE
+# # user = int(input("Welcome to the Work from Home Tracker. If you are a manager, type in the password"))
+# # if user == 1974:
+# #     prompt = input("Welcome, Manager. Please input what work week you want to view.")
+# #     # some kind of access here
 
-print(message)
+# # might be a good idea to store ID and employee names and check the input against it
+
+# employee_id = [123]
+# employee_register = []
+
+# while True:
+#     try:
+#         week_num = int(input("What work week are you logging?"))
+#         emp_id = int(input("What is your employee ID?"))
+#         emp_name = input("What is your full name?")
+
+#     except ValueError:
+#         print("Apologies. It seems as though your input is invalid.")
+#         continue
+#     if emp_id not in employee_id:
+#         print("Apologies. Your employee id is invalid.")
+#         continue
+#     else:
+#         break
+
+
+# while True:
+#     try:
+#         mon_hours = int(input("How many hours did you work on Monday?"))
+#         tue_hours = int(input("How many hours did you work on Tuesday?"))
+#         wed_hours = int(input("How many hours did you work on Wednesday?"))
+#         thu_hours = int(input("How mnany hours did you work on Thursday?"))
+#         fri_hours = int(input("How many hours did you work on Friday?"))
+#     except ValueError:
+#         print("Apologies. It seems like your input is invalid.")
+#         continue
+#     if (0 > mon_hours) or (24 < mon_hours):
+#         print("You must pick a number between 0 and 24.")
+#         continue
+#     else:
+#         break
+
+# total_hours = mon_hours + tue_hours + wed_hours + thu_hours + fri_hours
+# # try lop_id)
+# print(emp_name)
+# print("For work week {}".format(week_num))
+# print("You have worked {} hog the hours in a dictionary?
+# print(emurs".format(total_hours))
